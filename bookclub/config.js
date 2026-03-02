31f2caa8c91ae3c4a6693c290b04eded063eed0d
Simon F <sfuchs.de@gmail.com>
2026-03-02 13:29:53 -0500

    Update API_URL to the correct Web App URL

--- bookclub/config.js
+++ bookclub/config.js
@@ -1,4 +1,4 @@
 // Paste your Apps Script Web App URL here (ends with /exec)
 window.ECON_LUNCH = {
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE"
+  API_URL: "https://script.google.com/macros/s/AKfycbyje7hm9nvbilY_eAcLwPmvYgI0-030JoC8D9W9nfxMm_wvLxz6pOZeAYXq94FdEHFsxw/exec"
 };
